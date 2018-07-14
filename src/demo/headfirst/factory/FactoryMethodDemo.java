d0d607c72abf9b2c4fc26cbfeff8397745604370
chaolun <chaolun@outlook.com>
2018-07-14 10:47:47 +0800

    Initial commit 第二个测试

---
+++ src/demo/headfirst/factory/FactoryMethodDemo.java
@@ -0,0 +1,11 @@
+package demo.headfirst.factory;
+
+/**
+ * Created by chaolun on 2018/7/14.
+ * 设计模式：工厂方法
+ */
+public class FactoryMethodDemo {
+    public static void main(String[] args) {
+        System.out.println("demo");
+    }
+}
